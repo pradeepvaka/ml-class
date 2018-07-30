10c587f9b0ae13f575e65247d9334b06537d6626
pradeepreddyvaka <pradeepreddyvaka@local>
2018-07-30 20:14:29 +0000

    multi layer perceptron

--- keras-perceptron/perceptron-linear.py
+++ keras-perceptron/perceptron-linear.py
@@ -2,6 +2,8 @@ from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Dropout
 from keras.utils import np_utils
+from keras import regularizers
+from keras.layers import LeakyReLU
 
 import wandb
 from wandb.keras import WandbCallback
@@ -16,6 +18,9 @@ config = run.config
 img_width = X_train.shape[1]
 img_height = X_train.shape[2]
 
+X_train, X_test = X_train / 255.0, X_test / 255.0
+ 
+
 # one hot encode outputs
 y_train = np_utils.to_categorical(y_train)
 y_test = np_utils.to_categorical(y_test)
@@ -26,12 +31,13 @@ num_classes = y_train.shape[1]
 # create model
 model=Sequential()
 model.add(Flatten(input_shape=(img_width,img_height)))
-model.add(Dense(num_classes))
-model.compile(loss='mse', optimizer='adam',
+model.add(Dense(64))
+model.add(LeakyReLU(alpha=0.25))
+model.add(Dropout(0.30))
+model.add(Dense(num_classes,activation="softmax"))
+model.compile(loss='categorical_crossentropy', optimizer='adam',
                 metrics=['accuracy'])
 
 # Fit the model
 model.fit(X_train, y_train, epochs=10, validation_data=(X_test, y_test),
                     callbacks=[WandbCallback(validation_data=X_test, labels=labels)])
-
-
